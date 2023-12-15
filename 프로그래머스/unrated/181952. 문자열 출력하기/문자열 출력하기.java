0f1d4a64df8f2c978b8c73aef889117da14811a6
김태우 <120324961+txwxkx@users.noreply.github.com>
2023-12-15 18:32:19 +0900

    [level unrated] Title: 문자열 출력하기, Time: 149.85 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -4,10 +4,6 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        int len = a.length();
-        
-        if(len >= 1 && len <= 1000000) {
-            System.out.println(a);
-        }
+        System.out.print(a);
     }
 }
